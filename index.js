1aa9e21f45fd365a2b746e3119f2e2fe4a70cebc
Mira-amgad <149338007+Mira-amgad@users.noreply.github.com>
2023-11-20 16:25:23 +0200

    injected html forecast using JavaScript

--- index.js
+++ index.js
@@ -52,3 +52,28 @@ function submit(event) {
 let searchForm = document.querySelector("#form");
 searchForm.addEventListener("submit", submit);
 Search("Cairo");
+
+function weatherForecast() {
+  let forecast = document.querySelector("#forecast");
+  let days = ["Mon", "Tue", "Wed", "Thur", "Fri"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  forecast.innerHTML = forecastHtml;
+}
+weatherForecast();
